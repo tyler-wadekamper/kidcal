d5596d79cd063e0f33aed110f2b46daa6c62d539
tyler-wadekamper <wadekampert@spu.edu>
2023-06-03 14:56:32 -0700

    Commit first working prototype. Change clock and data pin numbers to match Rpi 3 model B pinout.

--- streaming.c
+++ streaming.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <pigpio.h>
 
-#define CLOCK_PIN 23
-#define DATA_PIN 24
+#define CLOCK_PIN 27
+#define DATA_PIN 22
 
 void setup() {
     if (gpioInitialise() < 0) {
@@ -12,22 +12,25 @@ void setup() {
     }
     gpioSetMode(CLOCK_PIN, PI_OUTPUT);
     gpioSetMode(DATA_PIN, PI_INPUT);
+    gpioWrite(CLOCK_PIN, 0);
 }
 
 int read_HX711() {
     int data = 0;
+    int count = 0;
     while (gpioRead(DATA_PIN)); // Wait for DATA_PIN to go low
-    for (int i = 0; i < 24; ++i) {
+    for (int i = 0; i < 26; i++) {
         gpioWrite(CLOCK_PIN, 1); // Send pulse
-        time_sleep(0.000001); // Wait a short time
         gpioWrite(CLOCK_PIN, 0); // End pulse
         data |= gpioRead(DATA_PIN) << (23 - i); // Read bit and add to data
+        count = i;
     }
     return data;
 }
 
 int main() {
     setup();
+    while (!gpioRead(DATA_PIN)); // Wait for DATA_PIN to go high
     while (1) {
         int weight = read_HX711();
         printf("Weight: %d\n", weight);
