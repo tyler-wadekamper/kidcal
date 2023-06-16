21a6094ed326ed204971c8c3563184eb6218e0c6
tyler-wadekamper <wadekampert@spu.edu>
2023-06-16 16:23:28 -0700

    Add conversion from raw data to pounds/ounces. Add tare functionality.

--- display-react-app/src/App.js
+++ display-react-app/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from "react";
 
+const CONVERSION_FACTOR = 49400;
+
 function App() {
   const [weight, setWeight] = useState(null);
+  const [tareOffset, setTareOffset] = useState(992000);
+  const [convertedWeight, setConvertedWeight] = useState({
+    pounds: 0,
+    ounces: 0,
+  });
   const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const weightInPounds = (weight - tareOffset) / CONVERSION_FACTOR;
+    const wholePounds = Math.floor(weightInPounds);
+    const ounces = Math.round((weightInPounds - wholePounds) * 16);
+
+    setConvertedWeight({
+      pounds: wholePounds,
+      ounces: ounces,
+    });
+  }, [weight, tareOffset]);
 
   useEffect(() => {
     const websocket = new WebSocket("ws://192.168.1.21:8080");
@@ -23,10 +41,22 @@ function App() {
     };
   }, []);
 
+  function handleTareScale(e) {
+    setTareOffset(weight);
+  }
+
   return (
-    <div>
-      <h1>Weight Data</h1>
-      {weight ? <p>Current weight: {weight}</p> : <p>No data yet...</p>}
+    <div class="container">
+      <h1>Kidcal</h1>
+      {weight ? (
+        <p>
+          {convertedWeight.pounds} lbs {convertedWeight.ounces} oz
+        </p>
+      ) : (
+        <p>No data yet...</p>
+      )}
+      <button onClick={handleTareScale}>Tare</button>
+      <button>Capture</button>
     </div>
   );
 }
